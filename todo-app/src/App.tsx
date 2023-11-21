82cc1f320b0bd1d3376d812841c75abec2be4e21
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-20 23:27:28 -0700

    updated app

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,33 +1,45 @@
 import React, { useEffect, useState } from 'react'
+import { Todo } from "./components/Todo";
 
 interface DataTodo {
-  completed: boolean;
-  id: number;
-  todo: string;
-  userId: number;
+ todos: {
+    completed: boolean;
+    id: number;
+    todo: string;
+    userId: number;
+  }[];
+  total: number;
+  skip: number;
+  limit: number;
 }
 
 export const App: React.FC = () => {
   const [name, setName] = useState("Steve");
   const [isActive, setIsActive] = useState(false);
-  const [todosList, setTodosList] = useState<DataTodo[]>([]);
+  const [todosList, setTodosList] = useState<DataTodo>();
 
   useEffect(() => {
     const getTodos = async () => {
       const data = await fetch("https://dummyjson.com/todos");
       const result  = await data.json();
-      console.log(result.todos);
-      setTodosList(result.todos);
+      console.log(result);
+      setTodosList(result); 
     };
     getTodos();
   }, [])
   
 
   return (
-    <div>{
-      todosList.map((todo) => {
-        return <div key={todo.id}>{todo.todo}</div>
-    })
-    }</div>
-  )
+    <div>
+      {todosList?.todos.map((todo) => {
+        return (
+           <Todo 
+           key={todo.id} 
+           todo={todo.todo} 
+           status={todo.completed ? "active":"inactive"}
+        />
+        );
+    })}
+    </div>
+  );
 };
