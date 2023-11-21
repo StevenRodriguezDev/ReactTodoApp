77d0036cf78e7eefce5aad0f76533da2ec906e8b
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-20 22:49:16 -0700

    updated app

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface DataTodo {
+  completed: boolean;
+  id: number;
+  todo: string;
+  userId: number;
 }
 
-export default App
+export const App: React.FC = () => {
+  const [name, setName] = useState("Steve");
+  const [isActive, setIsActive] = useState(false);
+  const [todosList, setTodosList] = useState<DataTodo[]>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const data = await fetch("https://dummyjson.com/todos");
+      const result  = await data.json();
+      console.log(result.todos);
+      setTodosList(result.todos);
+    };
+    getTodos();
+  }, [])
+  
+
+  return (
+    <div>{
+      todosList.map((todo) => {
+        return <div key={todo.id}>{todo.todo}</div>
+    })
+    }</div>
+  )
+};
